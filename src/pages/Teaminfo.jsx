2b8a44a4a4179d33070d01471a8644d5c632e983
anirudh <anirudh04mishra@gmail.com>
2023-09-21 20:14:44 +0530

    fix: team api link

--- src/pages/Teaminfo.jsx
+++ src/pages/Teaminfo.jsx
@@ -1,19 +1,47 @@
 /* eslint-disable react/prop-types */
+import { useEffect, useState } from 'react';
 import Layout from "../components/Layout";
+import axios from '../axios';
+import { toast } from 'react-toastify';
 
 export default function Teaminfo() {
 
-  function Member({ name, profile, leader }) {
+  const [teamName, setTeamName] = useState("Loading...")
+  const [members, setMembers] = useState(["Loading...",])
+
+  const getTeam = async () => {
+    try {
+      const res = await axios.get("/api/v1/teams/frontend-team", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+      })
+      console.log(res)
+      setTeamName(res?.data?.name)
+      setMembers(res?.data?.members)
+    } catch (error) {
+      setTeamName("Oops.")
+      setMembers(["Err..."])
+      toast.error(error?.response?.data?.detail)
+    }
+  }
+
+  useEffect(()=>{
+    getTeam()
+  }, [])
+
+
+  function Member({ name }) {
     return (
-      <div className="flex flex-row p-4">
-        <img
+      <div className="flex flex-row">
+        {/* <img
           src={profile}
           alt="Profile"
           className="w-16 h-16 rounded-full ml-8 mr-16"
-        />
+        /> */}
         <div className="flex flex-col">
           <h1 className="text-xl">{name}</h1>
-          <p className="text-sm">{leader ? "Teamleader" : ""}</p>
+          {/* <p className="text-sm">{leader ? "Teamleader" : ""}</p> */}
 
         </div>
       </div>
@@ -26,9 +54,9 @@ export default function Teaminfo() {
         <h1 className="w-max mt-3 mb-6 text-2xl">
           Manage Team
         </h1>
-        <button className="bg-transparent border-2 border-[#F04E29] hover:bg-red-700 px-5 py-1 text-xl font-bold rounded-sm text-[#F04E29] max-h-12 w-52 mt-2">
+        {/* <button className="bg-transparent border-2 border-[#F04E29] hover:bg-red-700 px-5 py-1 text-xl font-bold rounded-sm text-[#F04E29] max-h-12 w-52 mt-2">
           Delete Team
-        </button>
+        </button> */}
       </div>
 
       <div>
@@ -37,16 +65,19 @@ export default function Teaminfo() {
           Team name cannot be changed once created.
         </p>
         <div className="flex flex-row justify-between">
-          <input className="w-5/6 mx-3 py-3 my-6 pl-8 bg-[#411623] text-[#EAD3C1] rounded-md" type="text" placeholder="Alpha Design" />
-          <button className=" w-40 bg-red-600 hover:bg-red-700 mx-10 my-6 py-1 text-xl rounded-md">
+          <h1 className='text-xl'>{teamName}</h1>
+          {/* <input className="w-5/6 mx-3 py-3 my-6 pl-8 bg-[#411623] text-[#EAD3C1] rounded-md" type="text" placeholder="Alpha Design" /> */}
+          {/* <button className=" w-40 bg-red-600 hover:bg-red-700 mx-10 my-6 py-1 text-xl rounded-md">
             Save
-          </button>
+          </button> */}
         </div>
-        <h1 className="w-max mb-6 text-2xl">Team Members</h1>
-        <div className="flex flex-col p-6 mt-8  justify-around h-68">
-          <Member name="Member 1" profile="" leader={true} />
-          <Member name="Member 2" profile="" leader={false} />
-          <Member name="Member 3" profile="" leader={false} />
+        <h1 className="w-max mb-6 text-2xl mt-10">Team Members</h1>
+        <div className="flex flex-col">
+          {members.map((member, index) => {
+            return (
+              <Member name={member} key={`member${index}`} profile="" />
+            )
+          })}
         </div>
       </div>
 
