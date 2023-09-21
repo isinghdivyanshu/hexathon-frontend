89bb2e060c7927ff6d63233c9843abc52d517752
Rupaak Srinivas <Rupaaksrinivas8@gmail.com>
2023-09-21 22:24:09 +0530

    build: added problem statement logic

--- src/pages/Problem.jsx
+++ src/pages/Problem.jsx
@@ -1,16 +1,44 @@
 import Layout from "../components/Layout";
+import { useState, useEffect } from "react";
+import axios from '../axios'
+import { toast } from 'react-toastify';
 
 export default function Problem() {
+
+  const [psTitle, setPSTitle] = useState("")
+  const [psDescription, setPSDescription] = useState("")
+
+  const getPS = async () => {
+    try {
+      const res = await axios.get("/api/v1/problemStatements/team", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+      })
+      setPSDescription(res?.data?.description)
+      setPSTitle(res?.data?.name)
+      console.log(res.data)
+    } catch (error) {
+      toast.error(error?.response?.data?.detail)
+    }
+  }
+
+  useEffect(() => {
+    getPS()
+  }, [])
+
+
+
   return (
     <Layout title={"Problem Statement"}>
       <h1 className="w-full mt-3 mb-6 text-xl font-DelaGothicOne text-heading">
         Generate your problem statement
       </h1>
-      <div className="flex flex-col bg-[#752E324D] p-5 border-2 border-white rounded font-SpaceGrotesk">
-        <h1 className="text-xl mb-6 text-heading">
+      <div className="flex flex-col bg-[#752E324D] p-5 border-2 border-white rounded font-SpaceGrotesk ">
+        <h1 className={`text-xl mb-6 text-heading ${(psTitle==='' && psDescription==='') ? 'hidden':'block'}`}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </h1>
-        <div className="mb-10 text-sm text-content">
+        <div className={`mb-10 text-sm text-content ${(psTitle==='' && psDescription==='') ? 'hidden':'block'}`}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos debitis
           quasi vel eum alias, nam voluptatibus necessitatibus omnis error
           corrupti enim iste consectetur nostrum in officia quidem repudiandae
@@ -30,7 +58,7 @@ export default function Problem() {
           Generate New
         </button>
       </div>
-      <div className="width-full text-right my-4 ">
+      <div className={`width-full text-right my-4 ${(psTitle==='' && psDescription==='') ? 'hidden':'block'}`}>
         <span className="mx-6 text-sm text-info font-SpaceGrotesk font-semibold">
           Happy with your problem statement?
         </span>
