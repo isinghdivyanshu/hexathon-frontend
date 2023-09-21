4497af1fda7ea5bf7948491263d63c56ae9676c3
Divyanshu Singh <singhdivyanshu2707@gmail.com>
2023-09-22 01:11:33 +0530

    fix: modal for checkout

--- src/pages/market/Market.jsx
+++ src/pages/market/Market.jsx
@@ -1,32 +1,30 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import Layout from "../../components/Layout";
-import { useEffect, useState } from 'react';
-import axios from '../../axios';
-import { toast } from 'react-toastify';
-import Modal from 'react-modal';
-
+import { useEffect, useState } from "react";
+import axios from "../../axios";
+import { toast } from "react-toastify";
+import Modal from "react-modal";
 
 const customStyles = {
   content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    backgroundColor: "#752E324D",
-    width: "30%",
-    height: "30%",
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "#34101CCC",
+    width: "45%",
+    height: "auto",
+    border: "3px solid #58373D",
   },
   overlay: {
-    backgroundColor: "#250A19B2"
-  }
+    backgroundColor: "#250A19B2",
+  },
 };
 
 export default function Market() {
-
-
   const [typefaces, setTypefaces] = useState("Poppins, Inter");
   const [colors, setColors] = useState("Neo-pop");
   const [illustration, setIllustration] = useState("Isometric");
@@ -37,16 +35,16 @@ export default function Market() {
 
   const getCategories = async () => {
     try {
-      const response = await axios.get('/api/v1/categories', {
+      const response = await axios.get("/api/v1/categories", {
         headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
       });
       setCategories(response.data);
     } catch (error) {
       toast.error(error?.response?.data?.detail);
     }
-  }
+  };
 
   useEffect(() => {
     getCategories();
@@ -55,15 +53,15 @@ export default function Market() {
   // Function to open the modal
   const openCheckoutModal = () => {
     setIsCheckoutModalOpen(true);
-  }
+  };
 
   // Function to close the modal
   const closeCheckoutModal = () => {
     setIsCheckoutModalOpen(false);
-  }
+  };
 
   return (
-    <Layout title={'Marketplace'}>
+    <Layout title={"Marketplace"}>
       <div className="flex justify-between items-center mb-4">
         <div className="flex flex-col gap-1">
           <h1 className="text-xl font-DelaGothicOne text-heading">
@@ -86,7 +84,7 @@ export default function Market() {
         {categories?.map((cat, i) => {
           return (
             <Category name={cat.name} path={cat.id} key={`category${i}`} />
-          )
+          );
         })}
       </div>
 
@@ -97,22 +95,34 @@ export default function Market() {
         contentLabel="Confirmation Modal"
       >
         <div className="flex flex-col justify-between">
-          <h1 className="">Ready to Checkout?</h1>
-          <p>You can&apos;t enter and re-select elements from the marketplace once you are checked out. So please confirm your choices before you checkout.</p>
+          <h1 className="my-3 text-xl">Ready to Checkout?</h1>
+          <p className="mb-4 text-sm font-SpaceGrotesk text-info">
+            You can&apos;t enter and re-select elements from the marketplace
+            once you are checked out. So please confirm your choices before you
+            checkout.
+          </p>
           <div>
-            <h1 className="text-xl font-DelaGothicOne">
+            <h1 className="text-lg font-SpaceGrotesk text-medium">
               Typefaces - <span className="text-green-500">{typefaces}</span>
               <br />
               Colors - <span className="text-green-500">{colors}</span>
               <br />
-              Illustrations - <span className="text-green-500">{illustration}</span>
+              Illustrations -{" "}
+              <span className="text-green-500">{illustration}</span>
               <br />
               Theme - <span className="text-green-500">{theme}</span>
             </h1>
           </div>
-          <div className="flex justify-end">
-            <button onClick={closeCheckoutModal}>Cancel</button>
-            <button>Confirm</button>
+          <div className="flex w-full justify-end gap-4">
+            <button
+              onClick={closeCheckoutModal}
+              className="border-2 border-red-600 text-red-600 hover:border-red-700 hover:text-red-700 w-[20%] px-3 py-1 text-xs rounded-sm font-DelaGothicOne"
+            >
+              Cancel
+            </button>
+            <button className=" bg-red-600 hover:bg-red-700 w-[20%] px-3 py-1 text-xs text-white rounded-sm font-DelaGothicOne">
+              Confirm
+            </button>
           </div>
         </div>
       </Modal>
